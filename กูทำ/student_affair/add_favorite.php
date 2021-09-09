0abedffb75c004d086bb68b8afe34f37f81b6307
Peeranat Prasertpunt <77873202+arjzxc000@users.noreply.github.com>
2021-09-09 22:57:07 +0700

    Delete กูทำ directory

--- กูทำ/student_affair/add_favorite.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once("connection.php");
-//print_r($_GET);
-    $faverite_name=$_GET['favorite_name'];
-    $sql="INSERT INTO favorite(favorite_name) VALUES('$faverite_name')";
-//	echo $sql; 
- 	$result=mysqli_query($db,$sql);
-	header('Location: favorite.php');
-?>
--- กูทำ/student_affair/delete_favorite.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once("connection.php");
-//print_r($_GET);
-    $favorite_id=$_GET['favorite_id'];
-    $sql="DELETE FROM favorite WHERE favorite_id=$favorite_id";
-//	echo $sql; 
- 	$result=mysqli_query($db,$sql);
-	header('Location: favorite.php');
-?>
